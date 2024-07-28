10379c2a32d1e020fa570e30d02e0e0bedb6bf5d
asimmomin9380 <asimmominofficial@gmail.com>
2024-07-28 12:26:30 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -5,7 +5,7 @@ provider "aws"{
 resource "aws_instance" "AWSEC2INSTANCE"{
   ami= "ami-046d5130831576bbb"
 	instance_type="t2.micro"
-	key_name= devopsmay2024
+	key_name= "devopsmay2024"
 	security groups= ["Launch-wizard-2"]
 	tags={
 	  Name="terraformserver"
